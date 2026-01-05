5b5c424df7a95e1990e4934e38fc567dc21f04c7
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-01-05 13:19:45 +0100

    lidt bedere

--- ComponentsReact/src/Component/NavComponent/Nav.tsx
+++ ComponentsReact/src/Component/NavComponent/Nav.tsx
@@ -1,20 +1,17 @@
 import './Nav.css';
 
 export const Nav = () => {
-
     const links = ['Home', 'About', 'Services', 'Contact'];
 
     return (
         <nav>
-            <ul style={{ listStyle: 'none' }}>
-                <li>
-                    {links.map((link, index) => (
-                        <a key={index} href={`#${link.toLowerCase()}`} style={{ padding: '1rem', margin: '0 0.5rem' }}>
-                            {link}
-                        </a>
-                    ))}
-                </li>
+            <ul className="nav-list">
+                {links.map((link) => (
+                    <li key={link} className="nav-item">
+                        <a href={`#${link.toLowerCase()}`}>{link}</a>
+                    </li>
+                ))}
             </ul>
         </nav>
-    )
-}
+    );
+};
