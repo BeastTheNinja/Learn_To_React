eb3b67e960819ac30b1e36a38768d6e4564bfcbb
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-01-07 10:49:37 +0100

    hurtig opdatering

---
+++ PropsElements/src/component/MainComponent/MainComponent.tsx
@@ -0,0 +1,15 @@
+import { Greeting } from "../GreetingComponent/Greeting"
+
+export const MainContent = () => {
+    return (
+        <main className="bg-blue-500 flex-1 flex flex-col justify-center items-center p-4">
+            <h2 className="text-2xl text-white">Welcome to My Portfolio</h2>
+            <p className="mt-4 text-white text-center max-w-md">
+               <Greeting name="Sebastian Larsen" /> 
+               <Greeting name="Benjamin" /> 
+               <Greeting name="Wally" /> 
+               
+            </p>
+        </main>
+    )
+}
