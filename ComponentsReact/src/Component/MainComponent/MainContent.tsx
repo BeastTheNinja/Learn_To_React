90598cc6d27b41b8e83eddb22c8d2325b7401aad
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-01-07 13:41:11 +0100

    update

--- ComponentsReact/src/Component/MainComponent/MainContent.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Main.module.scss';
-
-export const MainContent = () => {
-    return (
-        <main className={styles.mainContent}>
-            <p>dette er main content</p>
-        </main>
-    )
-}
