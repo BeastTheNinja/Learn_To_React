5460252d3dbe777c2a94c701f1461828e3f23958
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-01-07 11:09:19 +0100

    update created liste med food og film

--- PropsElements/src/component/MainComponent/MainComponent.tsx
+++ PropsElements/src/component/MainComponent/MainComponent.tsx
@@ -1,15 +1,16 @@
 import { Greeting } from "../GreetingComponent/Greeting"
+import { FavoritesList } from "../ListeComponent/Liste"
 
 export const MainContent = () => {
-    return (
-        <main className="bg-blue-500 flex-1 flex flex-col justify-center items-center p-4">
-            <h2 className="text-2xl text-white">Welcome to My Portfolio</h2>
-            <p className="mt-4 text-white text-center max-w-md">
-               <Greeting name="Sebastian Larsen" /> 
-               <Greeting name="Benjamin" /> 
-               <Greeting name="Wally" /> 
-               
-            </p>
-        </main>
-    )
+  return (
+    <main className="bg-blue-500 flex-1 flex flex-col justify-center items-center p-4 min-h-screen">
+      <h2 className="text-2xl text-white font-bold">Lærings mål</h2>
+      <p className="mt-4 text-white text-center max-w-md">
+        <Greeting name="Sebastian Larsen" /> 
+        <Greeting name="Benjamin" /> 
+        <Greeting name="Wally" /> 
+      </p>
+      <FavoritesList />
+    </main>
+  )
 }
