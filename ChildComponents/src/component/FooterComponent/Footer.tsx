5359e1d58af52416fcddef909273e8ca20cfb5a5
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-01-08 09:19:00 +0100

    update

--- ChildComponents/src/component/FooterComponent/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-    return (
-        <footer className="bg-blue-600 p-4 text-center text-white">
-            <p>© 2026 Sebastian Larsen</p>
-        </footer>
-    )
-}
